5281b7ba8e0daa6a25bea1125e367e733b0307d5
peshk0v <nikita.peshkovvv@gmail.com>
2026-03-31 19:26:08 +0300

    Add dir chose win

--- core/app.py
+++ core/app.py
@@ -1,5 +1,60 @@
 import customtkinter as ctk
+import tkinter as tk
 import core
+
+class TitleFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.iconLb = ctk.CTkLabel(
+            self,
+            text="",
+            image=core.getImg((48, 48), master.man["Icon"])
+        )
+        self.iconLb.pack(side="left", padx=(15, 20), pady=15)
+
+        self.nameLb = ctk.CTkLabel(
+            self,
+            text=master.man["Name"],
+            fg_color="transparent",
+            font=ctk.CTkFont(size=18, weight="bold")
+        )
+        self.nameLb.pack(side="left", pady=15)
+
+        self.button = ctk.CTkButton(
+            self,
+            text="Install",
+            command=master.button_callbck,
+            width=100
+        )
+        self.button.pack(side="right", padx=(20, 15), pady=15)
+
+class DirFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.nameLb = ctk.CTkLabel(
+            self,
+            text=f"{master.man['Name']} - Dir",
+            fg_color="transparent",
+            font=ctk.CTkFont(size=18, weight="bold")
+        )
+        self.nameLb.pack(side="top", pady=10)
+        self.chose = ctk.CTkButton(self, text="Open", command=self.choseFile, width=50)
+        self.chose.pack(side="left", padx=15)
+        self.entryVar = tk.StringVar(value="C:/Program Files/")
+        self.dirEntr = ctk.CTkEntry(self, placeholder_text="Dir to app", textvariable=self.entryVar, width=200)
+        self.dirEntr.pack(side="left")
+
+        self.button = ctk.CTkButton(
+            self,
+            text="Next",
+            command=lambda: master.windws(0),
+            width=50
+        )
+        self.button.pack(side="left", padx=15)
+
+    def choseFile(self):
+        directory = tk.filedialog.askdirectory(title="Chose program dir:")
+        self.entryVar.set(directory)
 
 class App(ctk.CTk):
     def __init__(self):
@@ -7,34 +62,25 @@ class App(ctk.CTk):
         self.geometry("400x150")
         self.man = core.getMan()
 
-        self.card = ctk.CTkFrame(self, corner_radius=15)
-        self.card.pack(expand=True, fill="both", padx=20, pady=20)
+        self.windws(0)
 
-        self.iconLb = ctk.CTkLabel(
-            self.card, 
-            text="", 
-            image=core.getImg((48, 48), self.man["Icon"])
-        )
-        self.iconLb.pack(side="left", padx=(15, 20), pady=15)
-
-        self.nameLb = ctk.CTkLabel(
-            self.card, 
-            text=self.man["Name"], 
-            fg_color="transparent",
-            font=ctk.CTkFont(size=18, weight="bold")
-        )
-        self.nameLb.pack(side="left", pady=15)
-
-        self.button = ctk.CTkButton(
-            self.card, 
-            text="Install",
-            command=self.button_callbck,
-            width=100
-        )
-        self.button.pack(side="right", padx=(20, 15), pady=15)
+    def windws(self, stage):
+        if hasattr(self, 'frame1'):
+            self.frame1.destroy()
+        if hasattr(self, 'frame2'):
+            self.frame2.destroy()
+        self.update_idletasks()
+        
+        match stage:
+            case 0:
+                self.frame1 = TitleFrame(master=self)
+                self.frame1.pack(fill="both", expand=True, padx=20, pady=20)
+            case 1:
+                self.frame2 = DirFrame(master=self)
+                self.frame2.pack(fill="both", expand=True, padx=20, pady=20)
 
     def button_callbck(self):
-        print("Install oga")
+        self.after(10, lambda: self.windws(1))
 
 app = App()
 app.mainloop()
--- core/core.py
+++ core/core.py
@@ -1,14 +1,31 @@
 import json
 from PIL import Image
 import customtkinter as ctk
+from pathlib import Path
+import tkinter as tk
+
+BASE_DIR = Path(__file__).parent.absolute()
+
+def log(txt, args):
+    match args:
+        case 0:print(f"[LOG] - {txt}")
+        case 1: print(f"[WARNING] - {txt}")
+        case 2: print(f"[ERROR] - {txt}\nCreate issule on github!")
 
 def getMan():
-    with open("manifest.json", "r") as mf:
-        return json.load(mf)
+    with open(f"{BASE_DIR}/manifest.json", "r") as mf:
+        manf = json.load(mf)
+        log(f"Manifest loaded:\n{manf}", 0)
+        return manf
 
 def getImg(size, pathToImg, darkPathToImg=None):
-    img = Image.open(pathToImg)
+    log(f"Getting img ({pathToImg})", 0)
+    img = Image.open(BASE_DIR / pathToImg)
     if darkPathToImg is None:
         return ctk.CTkImage(light_image=img, dark_image=img, size=size)
     else:
-        return ctk.CTkImage(light_image=img, dark_image=Image.open(darkPathToImg), size=size)
+        return ctk.CTkImage(light_image=img, dark_image=Image.open(BASE_DIR / darkPathToImg), size=size)
+
+def install(method, dir, args=None):
+    match method:
+        case 1: pass
